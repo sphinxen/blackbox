e58a8f8c31bf04c87777b4542b758b3d49c8c0d3
Josef Karlsson <josef.karlsson@lsbolagen.se>
2019-11-30 23:43:24 +0100

    Change variable name (again)

--- BlackBox.php
+++ BlackBox.php
@@ -6,22 +6,22 @@ class BlackBox {
      * Generates a URL-encoded string with alternated query. It is possible to change and/or remove existing values from
      * the query.
      *
-     * @param mixed $keys Query argument key to modify. Only existing arguments will be affected.
-     * @param mixed $values New value of the query argument key. False, empty or identical value will result in removal of query argument.
-     * @param array $extra_arguments Additional new arguments to add to the query
+     * @param mixed $query_keys Query argument key to modify. Only existing arguments will be affected.
+     * @param mixed $new_values New value of the query argument key. False, empty or identical value will result in removal of query argument.
+     * @param array $extra_query_data Additional query to add. An associative array with query name and value.
      * @return string
      */
-    public function url($keys, $values = false, array $extra_arguments = []) {
-        $keys = (array) $keys;
-        $values = (array) $values ?: [];
+    public function url($query_keys, $new_values = false, array $extra_query_data = []) {
+        $query_keys = (array) $query_keys;
+        $new_values = (array) $new_values;
         $query = $_GET;
 
-        foreach ($keys as $index => $key) {
+        foreach ($query_keys as $index => $key) {
             if(isset($query[$key])) {
-                if ((empty($values[$index]) || $query[$key] == $values[$index])) {
+                if ((empty($new_values[$index]) || $query[$key] == $new_values[$index])) {
                     unset($query[$key]);
                 } else {
-                    $query[$key] = $values[$index];
+                    $query[$key] = $new_values[$index];
                 }
             }
         }
@@ -29,7 +29,7 @@ class BlackBox {
         $url =  $this->is_ssl() ? "https://" : "http://";
         $url .= $_SERVER['HTTP_HOST'];
         $url .= $_SERVER['SCRIPT_NAME'];
-        $url .= $query ? '?' . http_build_query($query + $extra_arguments) : '';
+        $url .= $query ? '?' . http_build_query($query + $extra_query_data) : '';
 
         return $url;
     }
